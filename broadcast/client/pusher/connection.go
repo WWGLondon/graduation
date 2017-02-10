f8ee7ccb33ee364015b54dfe335ee7f6953e8d60
Nic Jackson <jackson.nic@gmail.com>
2017-02-10 21:19:00 +0000

    Updated some documentation

--- broadcast/client/pusher/connection.go
+++ broadcast/client/pusher/connection.go
@@ -10,22 +10,27 @@ import (
 
 var listeners chan *Connection
 
+// init is a package level function to start the run loop
 func init() {
 	listeners = make(chan *Connection)
 	go runLoop()
 }
 
+// runLoop is an internal function to keep connection listeners alive
 func runLoop() {
 	for newListener := range listeners {
 		go newListener.listen()
 	}
 }
 
+// Connection is responsible for connecting to pusher and handling the receipt
+// of messages
 type Connection struct {
 	ws            *websocket.Conn
 	subscriptions map[string]chan InMessage
 }
 
+// Connect to the pusher server
 func (c *Connection) Connect(url *url.URL, origin string) error {
 	c.subscriptions = make(map[string]chan InMessage)
 
@@ -46,8 +51,10 @@ func (c *Connection) Connect(url *url.URL, origin string) error {
 	return nil
 }
 
+// Subscribe subscribes to a channel, when a message is received there will be
+// a new message pushed to the channel
 func (c *Connection) Subscribe(channel string) (chan InMessage, error) {
-	subscribeChannel := make(chan InMessage)
+	subscribeChannel := make(chan InMessage, 100) // use a buffered channel to avoid blocking
 
 	event := OutMessage{
 		Event: SubscribeEvent,
@@ -75,6 +82,7 @@ func (c *Connection) Subscribe(channel string) (chan InMessage, error) {
 	return subscribeChannel, nil
 }
 
+// listen is an internal method which is called by the run loop
 func (c *Connection) listen() {
 	var buffer = make([]byte, 512)
 
